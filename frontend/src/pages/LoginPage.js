d83fe3cd0df278c54f3fd80d1da562062b5ce9c8
aptheparker <aptheparker@gmail.com>
2023-07-15 21:38:03 +0900

    wait modal added

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,22 +1,20 @@
 // imports
 
 // components
-import NavigationBar from '../components/NavigationBar/NavigationBar'
-import Title from '../components/Title/Title'
-import GoogleLogin from '../components/GoogleLoginButton/GoogleLoginButton';
+import NavigationBar from "../components/NavigationBar/NavigationBar";
+import Title from "../components/Title/Title";
+import GoogleLogin from "../components/GoogleLoginButton/GoogleLoginButton";
 
 // css
-import './LoginPage.css';
+import "./LoginPage.css";
 
 function LoginPage() {
-  
-
   return (
-    <div className="login-page">
-      <NavigationBar/>
-      <Title text="Comity" fontSize={100} marginTop={200}/>
-      <GoogleLogin/>
-    </div>
+      <div className="login-page">
+        <NavigationBar />
+        <Title text="Comity" fontSize={100} marginTop={200} />
+        <GoogleLogin />
+      </div>
   );
 }
 
